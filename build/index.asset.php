b1f14070c50d8913b6bd4c118bc51a05024e1bfd
denischweya <denischweya@gmail.com>
2026-04-21 05:22:22 +0300

    fix: bump version to 1.0.17, resolve dashboard UI duplication, fix REST API download routing, and update pipeline batch size defaults.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2858ec4df41ae2b188bf');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'bc40f35f138d6575216f');
