f60936a7107d5c5fdfbb304678ca6c8e8ba62527
denischweya <denischweya@gmail.com>
2026-07-09 13:05:51 +0300

    Move pro features into free plugin

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '217f87c60bfed9a4396f');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b26fa2eec98ade3c4ea8');
